372f636ede4db16e41a130d91bde9b4147c7a693
Isabelle Hallman <isaha894@student.liu.se>
2018-09-17 15:07:51 +0200

    Sketch geometry solutions

--- Geometry.h
+++ Geometry.h
@@ -2,13 +2,23 @@
 // Created by Isabelle Hallman on 2018-09-17.
 //
 
-#include <glm/vec3.hpp>
+#include "glm/glm.hpp"
+
+struct Vertex;
+struct Direction;
+struct ColorDbl;
+struct Triangle;
+struct Ray;
+
 
 struct Vertex {
     Vertex(float xIn, float yIn, float zIn, float wIn)
-            : x(xIn), y(yIn), z(zIn), w(wIn) { }
-    vec3<float> position = vec3<float>(x, y, z);
+            : x(xIn), y(yIn), z(zIn), w(wIn) {
+        position = glm::vec3(x, y, z);
+    }
+
     float x, y, z, w;
+    glm::vec3 position;
 };
 
 struct Direction {
@@ -21,14 +31,15 @@ struct ColorDbl {
     float r, g, b;
 };
 
+
 struct Triangle {
     Triangle(Vertex &v1In, Vertex &v2In, Vertex &v3In, ColorDbl &colorIn)
             : v1(v1In), v2(v2In), v3(v3In), color(colorIn), normal(Direction(.0, .0, .0)) {
         computeNormal();
     }
 
-    void rayIntersection(Ray &arg) {
-
+    bool rayIntersection(Ray &arg) {
+        return true;
     }
 
     Vertex v1, v2, v3;
@@ -36,6 +47,16 @@ struct Triangle {
     Direction normal;
 
     void computeNormal() {
-        normal = Direction(0.0, .0, .0);
+        glm::cross((v1.position - v2.position), (v1.position - v3.position));
+        normal = Direction(.0, .0, .0);
     }
 };
+
+struct Ray {
+    Ray(Vertex* startIn, Vertex* endIn, ColorDbl* colorIn)
+            : startPoint(startIn), endPoint(endIn), color(colorIn), endPointTriangle(nullptr) { }
+    Vertex* startPoint, * endPoint;
+    ColorDbl* color;
+    Triangle* endPointTriangle;
+};
+
