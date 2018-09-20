81f6f31b0ead3e3e3a4f70183c7c0ed7e9793e57
Isabelle Hallman <isaha894@student.liu.se>
2018-09-20 11:40:28 +0200

    Code for testing rendering

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 // Created by Isabelle Hallman on 2018-09-17.
 //
 
-#include "Scene.h"
+#include "Camera.h"
 #include <list>
 #include <iostream>
 #include <vector>
@@ -12,14 +12,17 @@ int main() {
     Vertex v2 = Vertex(.0, .0, .1, .1);
     Vertex v3 = Vertex(.1, .0, .0, .2);
     ColorDbl color = ColorDbl(1., 1., 1.);
-    Ray myRay = Ray(&v1, &v2, &color);
-
+    Ray myRay = Ray(&v1, &v2);
 
     std::list<Triangle> triangles = {Triangle(v1, v2, v3, color)};
     Scene myScene = Scene(triangles);
 
     Triangle& intersectedTriangle = myScene.findIntersectedTriangle(myRay);
-    std::cout << intersectedTriangle.normal.vector.y << std::endl;
+    //std::cout << intersectedTriangle.normal.vector.y << std::endl;
+
+    Camera camera = Camera(&myScene);
+    camera.render();
+    camera.createImage();
     return 0;
 }
 
